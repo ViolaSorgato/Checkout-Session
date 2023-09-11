f64bda368ab00dbf87b6fdfd3ff3644533351f73
Viola Sorgato <egles.sorgato@medieinstitutet.se>
2023-09-11 21:30:50 +0200

    added logo

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,9 +1,12 @@
 import { Button } from "antd";
+import { Layout, Space } from "antd";
 import { ShoppingCartOutlined } from "@ant-design/icons";
+import Logo from "../../assets/Logo.png";
+
 export default function Header() {
   return (
     <>
-      {/* <img src={Logo} alt="Logo" /> */}
+      <img src={Logo} alt="Logo" />
       <Button type="primary">Login</Button>
       <ShoppingCartOutlined />
     </>
