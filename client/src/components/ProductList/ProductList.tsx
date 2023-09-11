5385316391c2b5468701f3812ccb736eed452ec0
Viola Sorgato <egles.sorgato@medieinstitutet.se>
2023-09-11 22:55:22 +0200

    improved style

--- client/src/components/ProductList/ProductList.tsx
+++ client/src/components/ProductList/ProductList.tsx
@@ -1,5 +1,27 @@
 import "./ProductList.css";
+import { Card, Col, Row } from "antd";
 
 export default function ProductList() {
-  return <div className="ProductList">Here I list my products</div>;
+  return (
+    <div className="ProductList">
+      <h3>Here I should map my products:</h3>
+      <Row gutter={16}>
+        <Col span={8}>
+          <Card title="Product" bordered={true}>
+            Card content
+          </Card>
+        </Col>
+        <Col span={8}>
+          <Card title="Product 2" bordered={true}>
+            Card content
+          </Card>
+        </Col>
+        <Col span={8}>
+          <Card title="Product 3" bordered={true}>
+            Card content
+          </Card>
+        </Col>
+      </Row>
+    </div>
+  );
 }
