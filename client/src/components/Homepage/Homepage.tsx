8cffd34b5002da675a27bcb88a61ce056d72744a
Viola Sorgato <egles.sorgato@medieinstitutet.se>
2023-09-11 22:56:00 +0200

    added maintcontent

--- client/src/components/Homepage/Homepage.tsx
+++ client/src/components/Homepage/Homepage.tsx
@@ -1,49 +1,11 @@
-import { useState } from "react";
-import ProductList from "../ProductList/ProductList";
 import Header from "../Header/Header";
-import { Button } from "antd";
-import "./Homepage.css";
+import MainContent from "../MainContent/MainContent";
 
 function Homepage() {
-  // example of hardcoded cart
-  const [cart, setCart] = useState([
-    {
-      product: "price_1NnhEZBAKZ3Nd1eYdvslLfvd",
-      quantity: 2,
-    },
-    {
-      product: "price_1Nnh6YBAKZ3Nd1eYY6dtbsNm",
-      quantity: 1,
-    },
-  ]);
-
-  async function handlePayment() {
-    const response = await fetch(
-      "http://localhost:3000/create-checkout-session",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(cart),
-      }
-    );
-
-    if (!response.ok) {
-      return;
-    }
-
-    const { url } = await response.json();
-    window.location = url;
-  }
-
   return (
     <>
       <Header />
-      <ProductList />
-      <Button className="CheckoutBtn" type="primary" onClick={handlePayment}>
-        To checkout
-      </Button>
+      <MainContent />
     </>
   );
 }
