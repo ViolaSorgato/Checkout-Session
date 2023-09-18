5f45ae0622546b8f5f827022e1181f51fe4c11eb
Viola Sorgato <egles.sorgato@medieinstitutet.se>
2023-09-18 12:13:40 +0200

    Didn't work, now it does again?

--- server/server.js
+++ server/server.js
@@ -42,7 +42,8 @@ app.post("/create-checkout-session", async (req, res) => {
         };
       }),
       customer: req.session.id,
-      mode: "subscription",
+      // mode: "subscription",
+      mode: "payment",
       success_url: "http://localhost:5173/confirmation",
       cancel_url: CLIENT_URL,
     });
