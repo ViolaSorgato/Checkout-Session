22a72d7706a2dc37f98cc829a929a1e13c3b35e4
Viola Sorgato <egles.sorgato@medieinstitutet.se>
2023-09-21 09:47:42 +0200

    Fixed confirmation page

--- client/src/components/Confirmationpage/Confirmationpage.tsx
+++ client/src/components/Confirmationpage/Confirmationpage.tsx
@@ -1,5 +1,20 @@
-function Confirmationpage() {
-  return <div>Thank you for your purchase!</div>;
-}
+import "./Confirmationpage.css";
+import { Button, Result } from "antd";
+import { Link } from "react-router-dom";
 
-export default Confirmationpage;
+export default function Confirmationpage() {
+  return (
+    <Result
+      status="success"
+      title="Hurray! We have received your payment!"
+      subTitle="Your order is being processed."
+      extra={[
+        <Link to="/">
+          <Button type="primary" className="buy-more-btn">
+            Buy some more
+          </Button>
+        </Link>,
+      ]}
+    />
+  );
+}
